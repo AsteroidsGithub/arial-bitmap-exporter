bae687efc0a48207b7a1086ebc893818926c25a2
Jonathan Voss <jv96@mahurangi.school.nz>
2022-10-24 02:00:30 +1300

    bugs: fixed canvas not being able to see exported node

--- code.ts
+++ code.ts
@@ -1,27 +1,30 @@
-figma.currentPage.selection.forEach(async (node) => {
+(async () => {
+  if (!figma.currentPage.selection[0]) return;
+
   // Prevents the plugin from remove data from the
   // original node
-  node = node.clone();
+  var node = figma.currentPage.selection[0].clone();
 
   // Flatten everything that isn't already a base shape
   if (node.type !== "RECTANGLE") node = figma.flatten([node]);
 
-  let tempBytes = await node.exportAsync({ format: "PNG" });
-  let temImage = figma.createImage(tempBytes);
+  var tempBytes = await node.exportAsync({ format: "PNG" });
+  var tempImage = figma.createImage(tempBytes);
 
-  // Destroy the first temp node
   node.remove();
 
-  node = figma.createRectangle();
-  node.fills = [{ type: "IMAGE", imageHash: temImage.hash, scaleMode: "FILL" }];
+  var tempNode = figma.createRectangle();
+  tempNode.fills = [
+    { type: "IMAGE", imageHash: tempImage.hash, scaleMode: "FILL" },
+  ];
 
   // Perform the appropriate transformation
   // to get a value that can be used easily
-  if ("resize" in node) node.resize(8, 12);
-  if ("rotation" in node) node.rotation = 0;
+  if ("resize" in tempNode) tempNode.resize(8, 12);
+  if ("rotation" in tempNode) tempNode.rotation = 0;
 
   // Exports the node as a encoded PNG
-  const image = await node.exportAsync();
+  const exportNode = await tempNode.exportAsync();
 
   // Create an invisible iframe to act as a "worker" which
   // will do the task of decoding and send us a message
@@ -34,9 +37,9 @@ figma.currentPage.selection.forEach(async (node) => {
   });
 
   // Send the raw bytes of the file to the worker.
-  figma.ui.postMessage(image);
+  figma.ui.postMessage(exportNode);
 
   // We need to remove the node because we created
   // it just to export the image
-  node.remove();
-});
+  tempNode.remove();
+})();
